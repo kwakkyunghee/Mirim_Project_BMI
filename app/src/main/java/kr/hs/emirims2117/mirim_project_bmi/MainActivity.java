cc20b149508f36a5b4aae4fd14617f6efed2b374
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-07-27 15:48:56 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirims2117/mirim_project_bmi/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirims2117.mirim_project_bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
